53ad5b0474bebf713ba543131e078882dae7f312
林柔捷 <112306059@g.nccu.edu.tw>
2026-01-01 17:11:51 +0800

    multiple NPC update

--- Assets/Script Folder/NPCController.cs
+++ Assets/Script Folder/NPCController.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
+
 public class NPCController : MonoBehaviour
 {
-    public string npc_role;           // 對應 npc_config.json 的角色名
-    public string configFileName;     // 對應 NPC 詳細設定 JSON
+    public string configFileName; // qianlong.json / huizong.json
 
     private NPCRequestManager requestManager;
 
@@ -14,7 +14,6 @@ public class NPCController : MonoBehaviour
 
     public void SendQuery(string query)
     {
-        // 先讀取該 NPC 的設定
         var npcInfo = requestManager.GetNPCInfoByConfig(configFileName);
         requestManager.SendNPCRequest(query, npcInfo);
     }
