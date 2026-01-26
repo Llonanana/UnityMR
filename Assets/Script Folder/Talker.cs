a74cd1a96ada59bfe08ddad44e7ea37bc7777805
林柔捷 <112306059@g.nccu.edu.tw>
2026-01-26 17:22:42 +0800

    實體區移到虛擬區

---
+++ Assets/Script Folder/Talker.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Talker : MonoBehaviour
+{
+    public TextToSpeech ttsManager;
+    public void Speak(string type)
+    {
+        // 動態產生路徑
+        string path = "Dialogues/" + type; // 對應 Resources/Dialogues/A_Type1.txt
+
+        TextAsset txt = Resources.Load<TextAsset>(path);
+
+        if (txt != null)
+        {
+            Debug.Log(txt.text);
+        }
+        else
+        {
+            Debug.LogWarning("找不到對應的 txt: " + path);
+        }
+    }
+}
