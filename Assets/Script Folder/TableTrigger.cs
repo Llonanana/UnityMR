aa933f61b8da833f125c594bc6770d8726e0a111
林柔捷 <112306059@g.nccu.edu.tw>
2026-01-26 18:00:20 +0800

    npc talk update
    Former-commit-id: 40ee40ff4530e05d58ef96f5efa862c305229440 [formerly 3c83cde778752e860c54f4699d6c6c1d36717f5b]
    Former-commit-id: 44bb17c829a0e0f7ae2ee82e3bb4ffd904a0e281

--- Assets/Script Folder/TableTrigger.cs
+++ Assets/Script Folder/TableTrigger.cs
@@ -1,13 +1,34 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TableTrigger : MonoBehaviour
 {
     public Talker npc;
+
+    // 記錄每個 TableItem 上次觸發時間
+    private Dictionary<TableItem, float> lastTriggerTime = new Dictionary<TableItem, float>();
+
+    public float triggerCooldown = 2f; // 冷卻時間，秒
+
     private void OnTriggerEnter(Collider other)
     {
         TableItem item = other.GetComponent<TableItem>();
         if (item == null) return;
 
+        // 檢查冷卻時間
+        if (lastTriggerTime.TryGetValue(item, out float lastTime))
+        {
+            if (Time.time - lastTime < triggerCooldown)
+            {
+                // 還沒超過冷卻，忽略這次碰撞
+                return;
+            }
+        }
+
+        // 更新上次觸發時間
+        lastTriggerTime[item] = Time.time;
+
+        // 觸發事件
         switch (item.itemType)
         {
             case ItemType.LianHuaWan:
